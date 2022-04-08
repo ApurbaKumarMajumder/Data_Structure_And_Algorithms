ca0f8df34f7e154383801e011e36d3d03a62684e
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-08 13:35:03 +0530

    total occurance

---
+++ Day 2/totalNumberOfOccurance.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOcc(int arr[], int key, int n)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        { // right meo jao
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        { // left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int key, int n){
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid])
+        { // right meo jao
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        { // left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    int even[11] = {1, 2, 3, 3, 3, 3, 3, 3, 4, 5, 6};
+
+    cout << "First Occurance of 3(key) is at index " << firstOcc(even, 3, 11)<<endl;
+    cout << "Last Occurance of 3(key) is at index " << lastOcc(even, 3, 11)<<endl;
+
+    int first = firstOcc(even, 3, 11);
+    int last = lastOcc(even, 3, 11);
+    int ans = last - first + 1;
+
+    cout<< "Total occurance of 3(key) is "<< ans <<endl;
+
+    return 0;
+}

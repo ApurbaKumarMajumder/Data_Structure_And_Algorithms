83527ca2cb45f3443bc3159cf64da7ce0538bfd4
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-11 16:25:52 +0530

    STL list

---
+++ C++ STL/list.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+int main(){
+
+    list<int> l;
+
+    l.push_back(10);
+    l.push_front(20);
+
+    for(int i:l){
+        cout<<i<<" ";
+    } cout<<endl;
+
+    l.erase(l.begin());
+    cout<<"after erase"<<endl;
+    for(int i:l){
+        cout<<i<<' ';
+    } cout<<endl;
+
+    cout<<"size of list "<<l.size()<<endl;
+
+    list<int> n(5, 100);
+
+    cout<<"Printing n"<<endl;
+    for(int i:n){
+        cout<<i<<' ';
+    } cout<<endl;
+}

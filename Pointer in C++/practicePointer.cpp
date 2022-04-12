75a95ed7154faa6fc81ea96b9a4ac69b93346b69
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-12 13:33:54 +0530

    more on pointer

---
+++ Pointer in C++/practicePointer.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /*
+    //pointer to int is created, and pointing to some garbage value
+    // int *p; // bad practice (so don't do any operations)
+
+    // int *p = 0; // null pointer
+    // cout<<*p<<endl;
+
+    int i = 5;
+    // int *p = &i;
+
+    int *p = 0; // initialization of pointer(null pointer)
+    p = &i; // reassigning pointer value to i's address
+
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    */
+
+   
+    return 0;
+}

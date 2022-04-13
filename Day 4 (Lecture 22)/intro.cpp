57d5b92b04c4bb6253dadd7f5c12d4ee194e27e9
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-13 14:53:17 +0530

    get maximum occuring character

--- Day 4 (Lecture 22)/intro.cpp
+++ Day 4 (Lecture 22)/intro.cpp
@@ -1,6 +1,41 @@
 #include<iostream>
 using namespace std;
 
+char getMaxOccCharacter(string s){
+
+    int arr[26] = {0}; // characher array of english alphabets
+
+    // create an array of count of characters
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+        //lowercase
+        int number = 0;
+        if(ch >= 'a' && ch <= 'z'){
+            number = ch - 'a';
+        }
+        else{
+            //uppercase
+            number = ch - 'A';
+        }
+        arr[number]++;
+    }
+
+    // finding maximum occuring character
+    int maxi = -1, ans = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (maxi < arr[i])
+        {
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    
+    char finalAns = 'a' + ans;
+    return finalAns;
+}
+
 bool checkPalindrome(char a[], int n){
     int s = 0;
     int e = n - 1;
@@ -33,23 +68,27 @@ int getLength(char name[]){
 
 int main(){
 
-    char name[20];
+    // char name[20];
 
-    cout<<"Enter your name "<<endl;
-    cin>>name;
-    // name[2] = '\0';
+    // cout<<"Enter your name "<<endl;
+    // cin>>name;
+    // // name[2] = '\0';
 
-    cout<<"Your name is ";
-    cout<<name<<endl;
+    // cout<<"Your name is ";
+    // cout<<name<<endl;
 
-    cout<<"Length: "<<getLength(name) <<endl;
+    // cout<<"Length: "<<getLength(name) <<endl;
 
-    reverse(name, getLength(name));
-    cout<<"Your name is "<<endl;
-    cout<<name<<endl;
+    // reverse(name, getLength(name));
+    // cout<<"Your name is "<<endl;
+    // cout<<name<<endl;
 
-    cout<< "Palindrome or Not: "<<checkPalindrome(name, getLength(name))<<endl;
-    // prints 1 or 0 for true and false
+    // cout<< "Palindrome or Not: "<<checkPalindrome(name, getLength(name))<<endl;
+    // // prints 1 or 0 for true and false
+
+    string s;
+    cin>>s;
+    cout<<getMaxOccCharacter(s)<<endl;
 
     
     return 0;

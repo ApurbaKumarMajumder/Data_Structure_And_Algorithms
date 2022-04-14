a2e39b1703c0120ab622ca53648e4be4ef286dbc
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-14 18:37:16 +0530

    charged with more content

--- OOPs/intro.cpp
+++ OOPs/intro.cpp
@@ -6,10 +6,34 @@ using namespace std;
 // this is a class
 class Hero {
     
+    
     // Properties
-    char name[100];
+    private: 
+    // can be accessed in the class only
     int health;
+    
+    public: // can be accessed in and outside of the class
     char level;
+
+    void print(){
+        cout<< level << endl;
+    }
+
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+
+    void setHealth(int h){
+        health = h;
+    }
+
+    void setLevel(char ch){
+        level = ch;
+    }
 
 };
 
@@ -17,6 +41,15 @@ int main(){
 
     // created a object
     Hero Apurba;
+    cout<<"Apurba health is "<<Apurba.getHealth()<<endl;
 
-    cout<<"size : "<<sizeof(Apurba)<<endl;
+    // Apurba.health = 100;
+    // use setter as it's private
+    Apurba.setHealth(100);
+    Apurba.level = 'A';
+
+    cout<<"health is: "<<Apurba.getHealth() <<endl;
+    cout<<"level is: "<<Apurba.level <<endl;
+
+    // cout<<"size : "<<sizeof(Apurba)<<endl;
 }

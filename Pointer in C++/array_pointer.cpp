a6c99e35648665a4eac71aad1abb5283dd46e95a
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-15 11:58:59 +0530

    pointer character array

--- Pointer in C++/array_pointer.cpp
+++ Pointer in C++/array_pointer.cpp
@@ -3,43 +3,44 @@ using namespace std;
 
 int main(){
 
-    // int arr[10] = {2, 5, 6};
+    /*
+    int arr[10] = {2, 5, 6};
 
-    // cout<<"address of first memory block is "<<arr<<endl;
-    // cout<<arr[0]<<endl;
-    // cout<<"address of first memory block is "<<&arr[0]<<endl;
+    cout<<"address of first memory block is "<<arr<<endl;
+    cout<<arr[0]<<endl;
+    cout<<"address of first memory block is "<<&arr[0]<<endl;
 
-    // cout<<"*arr -> "<<*arr<<endl;
-    // cout<<"*arr + 1 -> "<<*arr + 1<<endl;
-    // cout<<"(*arr + 1) -> "<<*(arr + 1)<<endl;
+    cout<<"*arr -> "<<*arr<<endl;
+    cout<<"*arr + 1 -> "<<*arr + 1<<endl;
+    cout<<"(*arr + 1) -> "<<*(arr + 1)<<endl;
 
-    // int arr[10] = {23, 122, 41, 67};
+    int arr[10] = {23, 122, 41, 67};
 
-    // cout<<"address of first memory block is "<<arr<<endl;
-    // cout<<arr[0]<<endl;
-    // cout<<"address of first memory block is "<<&arr[0]<<endl;
+    cout<<"address of first memory block is "<<arr<<endl;
+    cout<<arr[0]<<endl;
+    cout<<"address of first memory block is "<<&arr[0]<<endl;
 
-    // cout<<"*arr -> "<<*arr<<endl;
-    // cout<<"*arr + 1 -> "<<*arr + 1<<endl;
-    // cout<<"(*arr + 1) -> "<<*(arr + 1)<<endl;
-    // cout<<"*(arr) + 1 -> "<<*(arr) + 1<<endl;
-    // cout<<"arr[2] -> "<<arr[2]<<endl;
-    // cout<<"*(arr + 2) -> "<<*(arr + 2)<<endl;
+    cout<<"*arr -> "<<*arr<<endl;
+    cout<<"*arr + 1 -> "<<*arr + 1<<endl;
+    cout<<"(*arr + 1) -> "<<*(arr + 1)<<endl;
+    cout<<"*(arr) + 1 -> "<<*(arr) + 1<<endl;
+    cout<<"arr[2] -> "<<arr[2]<<endl;
+    cout<<"*(arr + 2) -> "<<*(arr + 2)<<endl;
 
-    // int i = 3;
-    // cout<<"i = 3, i[arr] -> " <<i[arr] <<endl;
+    int i = 3;
+    cout<<"i = 3, i[arr] -> " <<i[arr] <<endl;
 
-    // int temp[10] = {1, 2};
-    // cout<<sizeof(temp)<<endl;
+    int temp[10] = {1, 2};
+    cout<<sizeof(temp)<<endl;
 
-    // cout<<sizeof(temp)<<endl;
-    // cout<<"1st "<<sizeof(*temp)<<endl;
-    // cout<<"2nd "<<sizeof(&temp)<<endl;
+    cout<<sizeof(temp)<<endl;
+    cout<<"1st "<<sizeof(*temp)<<endl;
+    cout<<"2nd "<<sizeof(&temp)<<endl;
 
-    // int *ptr = &temp[0];
-    // cout<<sizeof(ptr)<<endl;
-    // cout<<sizeof(*ptr)<<endl; //value of
-    // cout<<sizeof(&ptr)<<endl; //address of
+    int *ptr = &temp[0];
+    cout<<sizeof(ptr)<<endl;
+    cout<<sizeof(*ptr)<<endl; //value of
+    cout<<sizeof(&ptr)<<endl; //address of
 
     int a[20] = {1, 2, 3, 5};
     cout<<"-> "<<&a[0]<<endl;
@@ -50,6 +51,7 @@ int main(){
     cout<<p<<endl;
     cout<<*p<<endl;
     cout<<"-> "<<&p<<endl;
+    */
 
     return 0;
 }

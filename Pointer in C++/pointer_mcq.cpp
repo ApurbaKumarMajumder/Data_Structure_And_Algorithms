85fdb3900689648d76080d97156c5b317547dae1
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-17 01:14:59 +0530

    5 mcq questions

--- Pointer in C++/pointer_mcq.cpp
+++ Pointer in C++/pointer_mcq.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
@@ -19,10 +19,24 @@ int main(){
     cout<<first<<endl;
     */
 
-   int first = 8;
-   int *p = &first;
-   cout<< (*p)++ << endl;
-   cout<<first<<endl;
+//    int first = 8;
+//    int *p = &first;
+//    cout<< (*p)++ << endl;
+//    cout<<first<<endl;
+
+    // int first = 8;
+    // int second = 11;
+    // int *third = &second;
+    // first = *third;
+    // *third = *third + 2;
+    // cout<< first << ' '<<second <<endl;
+
+    // float f = 12.5;
+    // float p = 21.5;
+    // float* ptr = &f; // address of f
+    // (*ptr)++; // value of f
+    // *ptr = p;
+    // cout<< *ptr << ' ' << f << " " << p <<endl;
 
     return 0;
 }

2acfd7032ac0399f398dd0dc571bb32c5a05c4b6
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-16 22:51:27 +0530

    more on double pointer

--- Pointer in C++/doublePointer.cpp
+++ Pointer in C++/doublePointer.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
-    
+
+    int i = 5;
+    int *ptr = &i;
+    int ** ptr2 = &ptr;
+
+    cout<<endl<<endl<<"Sub sahi chal rha h "<<endl<<endl;
+
+    cout<<"printing ptr "<<ptr <<endl;
+    cout<<"address of i "<<&i <<endl;
+
+    cout<<"address of ptr "<<&ptr<<endl;
+    cout<<"printing ptr2 "<<ptr2 <<endl;
+
+    cout<<"address of ptr2 "<<&ptr2<<endl;
+
+
+    cout<< *ptr2 <<endl;
+
+    // value of i
+    cout<< i<<endl;
+    cout<<*ptr<<endl;
+    cout<<**ptr2<<endl;
+
+    return 0;
 }

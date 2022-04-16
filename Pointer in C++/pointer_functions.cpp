f9ab87ffc53a3a47509488c0063768bcf54a0715
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-16 22:33:37 +0530

    more on function

--- Pointer in C++/pointer_functions.cpp
+++ Pointer in C++/pointer_functions.cpp
@@ -15,15 +15,32 @@ void update(int *p){
 
 }
 
+int getSum(int *arr, int n){
+
+    cout<<endl<<"Size : "<<sizeof(arr)<<endl;
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
 int main(){
 
-    int value = 5;
-    int *p = &value;
+    // int value = 5;
+    // int *p = &value;
 
-    //print(p);
-    cout<<"Before "<<*p <<endl;
-    update(p);
-    cout<<"After "<<*p<<endl;
+    // //print(p);
+    // cout<<"Before "<<*p <<endl;
+    // update(p);
+    // cout<<"After "<<*p<<endl;
+
+    int arr[6] = {1, 2, 3, 4, 5, 8};
+
+    cout<<"sum is "<<getSum(arr+3, 3)<<endl;
 
     return 0;
 }

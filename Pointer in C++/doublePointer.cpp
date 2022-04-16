d1afbec42c2a8879ac09b67730bbed3a92fdb7f1
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-16 23:34:23 +0530

    practice double pointer

--- Pointer in C++/doublePointer.cpp
+++ Pointer in C++/doublePointer.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void update(int **ptr2){
+    // ptr2 = ptr2 + 1;
+    //kuch change hoga kya -> NO
+
+    // *ptr2 = *ptr2 + 1;
+    // kuch change hoga kya -> YES
+
+    **ptr2 = **ptr2 + 1;
+    // kuch change hoga -> YES
+}
+
 int main(){
 
     int i = 5;
     int *ptr = &i;
     int ** ptr2 = &ptr;
 
+    /*
     cout<<endl<<endl<<"Sub sahi chal rha h "<<endl<<endl;
 
     cout<<"printing ptr "<<ptr <<endl;
@@ -24,6 +36,40 @@ int main(){
     cout<< i<<endl;
     cout<<*ptr<<endl;
     cout<<**ptr2<<endl;
+
+    //printing address of i
+    cout<<&i<<endl;
+    cout<<ptr<<endl;
+    cout<<*ptr2<<endl;
+    */
+
+   
+   cout<<endl;
+   cout<<"before "<<i<<endl;
+   cout<<"before "<<ptr <<endl;
+   cout<<"before "<<ptr2<<endl;
+   update(ptr2);
+   cout<<"after "<< i<<endl;
+   cout<<"after "<<ptr <<endl;
+   cout<<"after "<<ptr2<<endl;
+
+//    cout<<endl;
+//    cout<<"before "<<i<<endl;
+//    cout<<"before "<<ptr <<endl;
+//    cout<<"before "<<ptr2<<endl;
+//    update(ptr2);
+//    cout<<"after "<< i<<endl;
+//    cout<<"after "<<ptr <<endl;
+//    cout<<"after "<<ptr2<<endl;
+
+//    cout<<endl;
+//    cout<<"before "<<i<<endl;
+//    cout<<"before "<<ptr <<endl;
+//    cout<<"before "<<ptr2<<endl;
+//    update(ptr2);
+//    cout<<"after "<< i<<endl;
+//    cout<<"after "<<ptr <<endl;
+//    cout<<"after "<<ptr2<<endl;
 
     return 0;
 }

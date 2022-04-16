09aa1ff0ba73a7c08c62e300386a371b70bdf15e
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-17 00:09:49 +0530

    3 mcq question

--- Pointer in C++/pointer_mcq.cpp
+++ Pointer in C++/pointer_mcq.cpp
@@ -3,10 +3,26 @@ using namespace std;
 
 int main(){
 
-    int first = 8;
-    int second = 18;
-    int *ptr = &second;
-    cout<<first<<' '<<second<<endl;
+    // int first = 8;
+    // int second = 18;
+    // int *ptr = &second;
+    // *ptr = 9;
+    // cout<<first<<' '<<second<<endl;
+
+    /*
+    int first = 6;
+    int *p = &first;
+    int *q = p;
+    // cout<<q<<endl;
+    // cout<<*q<<endl;
+    (*q)++;
+    cout<<first<<endl;
+    */
+
+   int first = 8;
+   int *p = &first;
+   cout<< (*p)++ << endl;
+   cout<<first<<endl;
 
     return 0;
 }

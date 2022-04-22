b3e7546d59a019ca46932c4d2da7daa6fed4924f
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-22 18:11:21 +0530

    ref type and call by value

--- pepcoding pointer/pass_by_value_refrence.cpp
+++ pepcoding pointer/pass_by_value_refrence.cpp
@@ -13,6 +13,18 @@ void exp2(int *r1, int *r2){
     cout<<*r1<<" "<<*r2<<endl;
 }
 
+void exp3(int* p1, int*p2){
+
+    cout<<p1<<" "<<p2<<endl;
+    cout<<*p1<<" "<<*p2<<endl;
+
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    cout<<*p1<<" "<<*p2<<endl;
+}
+
 int main(){
 
     //val types, call by value
@@ -21,7 +33,13 @@ int main(){
     // exp(v1, v2);
 
     //val types, call by reference
-    cout<<v1<<' '<<v2<<endl;
-    exp2(&v1, &v2);
-    cout<<v1<<' '<<v2<<endl;
+    // cout<<v1<<' '<<v2<<endl;
+    // exp2(&v1, &v2);
+    // cout<<v1<<' '<<v2<<endl;
+
+    //ref types, call by value
+    int* r1 = &v1;
+    int* r2 = &v2;
+    exp3(r1, r2);
+    cout<<v1<<" "<<v2<<endl;
 }

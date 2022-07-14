ea52e74aeec56324008b1d6476cdd877d29d3e0e
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-14 21:22:22 +0530

    Insertion at start and end in LL

--- Linked List/singlyLinkedList.cpp
+++ Linked List/singlyLinkedList.cpp
@@ -13,10 +13,57 @@ class Node {
     }
 };
 
-int main() {
-    Node* node1 = new Node(10);
-    cout<< node1 -> data <<endl;
-    cout<< node1 -> next <<endl;
+void InsertAtTail(Node* & tail, int data) {
 
+    // new Node create
+    Node* temp = new Node(data);
+    tail -> next = temp;
+    tail = tail -> next;
+}
+
+void InsertAtHead(Node* &head, int data) {
+    // create a new node
+    Node* temp = new Node(data);
+    temp -> next = head;
+    head = temp;
+}
+
+void printLL(Node* & head) {
+    // pointing the current element
+    Node* temp = head;
+
+    while (temp != NULL) {
+        cout<< temp->data <<" ";
+        temp = temp -> next;
+    }
+}
+
+int main() {
+
+    // create a new Node
+    Node* node1 = new Node(10);
+    // cout<< node1 -> data <<endl;
+    // cout<< node1 -> next <<endl;
+
+    Node* head = node1;
+    Node* tail = node1;
+    // head pointed to node1
+    // printLL(head);
+    // cout<<" "<<endl;
+
+    // InsertAtHead(head, 12);
+    // printLL(head);
+    // cout<<" "<<endl;
+
+    // InsertAtHead(head, 15);
+    // printLL(head);
+
+    // tail pointed to node1
+    InsertAtTail(tail, 12);
+    printLL(head);
+    cout<<" "<<endl;
+
+    InsertAtTail(tail, 15);
+    printLL(head);
     return 0;
 }

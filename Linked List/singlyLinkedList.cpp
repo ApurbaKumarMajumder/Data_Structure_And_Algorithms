37109d124be7bd40b2401866a3799a3dc6f706e4
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-15 12:49:29 +0530

    insert and delete node in LL

--- Linked List/singlyLinkedList.cpp
+++ Linked List/singlyLinkedList.cpp
@@ -11,6 +11,17 @@ class Node {
         this -> data = data;
         this -> next = NULL;
     }
+
+    // destructor
+    ~Node() {
+        int value = this -> data;
+        // memory free
+        if (this -> next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout<<"memory id free for node with data "<<value<<endl;
+    }
 };
 
 void InsertAtTail(Node* & tail, int data) {
@@ -35,6 +46,60 @@ void printLL(Node* & head) {
     while (temp != NULL) {
         cout<< temp->data <<" ";
         temp = temp -> next;
+    }
+}
+
+void insertAtPosition(Node* &head, Node* &tail, int position, int data) {
+    
+    // insert at start
+    if(position == 1) {
+        InsertAtHead(head, data);
+        return;
+    }
+
+    Node* temp = head;
+    int count = 1;
+
+    while(count < position - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+    // inserting at the Last position
+    if(temp -> next == NULL) {
+        InsertAtTail(tail, data);
+        return;
+    }
+
+    // creating a node for data
+    Node* temp2 = new Node(data);
+    temp2->next = temp->next;
+    temp->next = temp2;
+}
+
+void deleteNode(int position, Node* &head){
+    if (position == 1) {
+        // deleting first(starting) node
+        Node* temp = head;
+        head = head -> next;
+        // memory free start node
+        temp->next = NULL;
+        delete temp;
+    } 
+    else {
+        // deleting any middle node or last node
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int count = 1;
+        while (count < position) {
+            prev = curr;
+            curr = curr->next;
+            count++;
+        }
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
     }
 }
 
@@ -65,5 +130,14 @@ int main() {
 
     InsertAtTail(tail, 15);
     printLL(head);
+    cout<<" "<<endl;
+
+    insertAtPosition(head, tail, 2, 22);
+    printLL(head);
+    cout<<" "<<endl;
+
+    deleteNode(3, head);
+    printLL(head);
+    cout<<" "<<endl;
     return 0;
 }

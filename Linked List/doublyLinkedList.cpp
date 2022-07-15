467b42e9670c7a5bbaefd1f8dbda773d317b694b
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-15 23:40:04 +0530

    insert at head function implemented

--- Linked List/doublyLinkedList.cpp
+++ Linked List/doublyLinkedList.cpp
@@ -9,7 +9,7 @@ class Node {
 
     // constructor
     Node(int val) {
-        this->data = val
+        this->data = val;
         this->prev = NULL;
         this->next = NULL;
     }
@@ -20,13 +20,43 @@ void printLL(Node* &head) {
 
     while (temp != NULL)
     {
-        cout<<temp->data<<endl;
+        cout<<temp->data<<" ";
         temp = temp->next;
     }
     cout<<"\n";
 }
 
+int length(Node* head) {
+    int result = 0;
+    Node* temp = head;
+
+    while (temp != NULL)
+    {
+        result++;
+        temp = temp->next;
+    }
+    return result;
+}
+
+void insertAtHead(Node* &head, int val) {
+    Node* temp = new Node(val);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+
 int main() {
-    
+    Node* node1 = new Node(10);
+    Node* head = node1;
+
+    insertAtHead(head, 12);
+    printLL(head);
+
+    insertAtHead(head, 11);
+    printLL(head);
+
+    insertAtHead(head, 8);
+    printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
     return 0;
 }

090a2f763229b158dec33d442d22e9247eed3844
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-16 13:25:47 +0530

    insert at any position in DLL

--- Linked List/doublyLinkedList.cpp
+++ Linked List/doublyLinkedList.cpp
@@ -38,24 +38,107 @@ int length(Node* head) {
     return result;
 }
 
-void insertAtHead(Node* &head, int val) {
-    Node* temp = new Node(val);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+void insertAtHead(Node* &head, Node* &tail, int val) {
+    
+    // empty list
+    if (head == NULL) {
+        Node* temp = new Node(val);
+        head = temp;
+        tail = temp;
+    } else {
+        Node* temp = new Node(val);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+
+void insertAtTail(Node* &head, Node* &tail, int val) {
+
+    // empty list
+    if (tail == NULL) {
+        Node* temp = new Node(val);
+        tail = temp;
+        head = temp;
+    } else {
+        Node* temp = new Node(val);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }  
+}
+
+void insertAtPosition(Node* &head, Node* &tail, int position, int val, int size) {
+    
+    // insert at start
+    if (position == 1) {
+        insertAtHead(head, tail, val);
+        return;
+    } else if (position < 0 || position > size) {
+        // cannot insert element these areas
+        cout<<"Invalid arguments"<<"\n";
+        return;
+    }
+
+    Node* temp = head;
+    int count = 1;
+
+    while(count < position - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+    // insertion at the last position
+    if(temp -> next == NULL) {
+        insertAtTail(head, tail, val);
+        return;
+    }
+
+    // creating a node for new element at arbitary position
+    Node* nodeToInsert = new Node(val);
+
+    nodeToInsert->next = temp->next;
+    temp->next->prev = nodeToInsert;
+    temp->next = nodeToInsert;
+    nodeToInsert->prev = temp;
 }
 
 int main() {
-    Node* node1 = new Node(10);
-    Node* head = node1;
+    // Node* head = node1;
+    // Node* tail = node1;
 
-    insertAtHead(head, 12);
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtHead(head, tail, 12);
     printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
 
-    insertAtHead(head, 11);
+    insertAtHead(head, tail, 11);
     printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
 
-    insertAtHead(head, 8);
+    insertAtHead(head, tail, 8);
+    printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
+
+    insertAtTail(tail, tail, 22);
+    printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
+
+    insertAtPosition(head, tail, 2, 100, length(head));
+    printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
+
+    insertAtPosition(head, tail, 1, 101, length(head));
+    printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
+
+    insertAtPosition(head, tail, 6, 105, length(head));
+    printLL(head);
+    cout<<"Size of LL: "<<length(head)<<"\n";
+
+    insertAtPosition(head, tail, 7, 102, length(head));
     printLL(head);
     cout<<"Size of LL: "<<length(head)<<"\n";
     return 0;

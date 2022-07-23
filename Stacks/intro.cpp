5e87024506d5936275cee1c30551a670f8414815
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-23 20:44:04 +0530

    starting with stack implementation

--- Stacks/intro.cpp
+++ Stacks/intro.cpp
@@ -2,6 +2,33 @@
 #include<stack>
 using namespace std;
 
+class Stack {
+
+    // properties
+    public:
+        int *arr;
+        int top;
+        int size;
+
+    // behaviour
+    Stack(int size) {
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element) {
+        if (size - top > 1) {
+            top++;
+            arr[top] = element;
+        } else {
+            cout << "Stack OverFlow " << endl;
+        }
+    }
+
+    
+}
+
 int main() {
 
     // creation of stack
@@ -22,7 +49,7 @@ int main() {
         cout<<"Stack is not empty "<<endl;
     }
 
-    cout<<"Size of stack is "<<s.size();
+    cout<<"Size of stack is "<<s.size()<<"\n";
 
     return 0;
 }

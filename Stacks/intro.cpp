9bd70d6ee981fc965f3be806df37b28c7d21d17d
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-23 20:58:23 +0530

    working with our created stack

--- Stacks/intro.cpp
+++ Stacks/intro.cpp
@@ -1,78 +1,126 @@
-#include<iostream>
+#include <iostream>
 #include<stack>
 using namespace std;
 
-class Stack {
+class Stack
+{
 
     // properties
-    public:
-        int *arr;
-        int top;
-        int size;
+public:
+    int *arr;
+    int top;
+    int size;
 
     // behaviour
-    Stack(int size) {
-        this -> size = size;
+    Stack(int size)
+    {
+        this->size = size;
         arr = new int[size];
         top = -1;
     }
 
-    void push(int element) {
-        if (size - top > 1) {
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
             top++;
             arr[top] = element;
-        } else {
+        }
+        else
+        {
             cout << "Stack OverFlow " << endl;
         }
     }
 
-    void pop() {
-        if (top >= 0) {
+    void pop()
+    {
+        if (top >= 0)
+        {
             top--;
-        } else {
+        }
+        else
+        {
             cout << "Stack UnderFlow " << endl;
         }
     }
 
-    int peek() {
-        if (top >= 0) {
+    int peek()
+    {
+        if (top >= 0)
+        {
             return arr[top];
-        } else {
+        }
+        else
+        {
             cout << "Stack is Empty " << endl;
             return -1;
         }
     }
 
-    bool isEmpty() {
-        if (top == -1) {
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
 };
 
-int main() {
+int main()
+{
 
-    // creation of stack
-    stack<int> s;
+    Stack st(5);
 
-    // push operation
-    s.push(2);
-    s.push(3);
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(36);
+    st.push(26);
+    st.push(16);
 
-    // pop
-    s.pop();
+    cout << st.peek() << endl;
 
-    cout << "Printing top element "<<s.top()<<endl;
+    st.pop();
 
-    if (s.empty()) {
-        cout<< "Stack is empty "<<endl;
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout<<st.peek()<<endl;
+
+    if (st.isEmpty()) {
+        cout<< "Stack is empty mere dost"<< endl;
     } else {
-        cout<<"Stack is not empty "<<endl;
+        cout<< "Stack is not empty mere dost"<<endl;
     }
 
-    cout<<"Size of stack is "<<s.size()<<"\n";
+    // // creation of stack
+    // stack<int> s;
+
+    // // push operation
+    // s.push(2);
+    // s.push(3);
+
+    // // pop
+    // s.pop();
+
+    // cout << "Printing top element "<<s.top()<<endl;
+
+    // if (s.empty()) {
+    //     cout<< "Stack is empty "<<endl;
+    // } else {
+    //     cout<<"Stack is not empty "<<endl;
+    // }
+
+    // cout<<"Size of stack is "<<s.size()<<"\n";
 
     return 0;
 }

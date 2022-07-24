21098ed125d66cb6c7ca7368e5b5518a297dccb5
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-25 02:22:59 +0530

    Implement Deque question done

--- Queue/intro.cpp
+++ Queue/intro.cpp
@@ -5,7 +5,32 @@ using namespace std;
 int main()
 {
 
+    deque<int> d;
+
+    d.push_front(12);
+    d.push_back(14);
+
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+
+    d.pop_front();
+
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+
+    d.pop_back();
+
+    if (d.empty())
+    {
+        cout << "queue is empty" << endl;
+    }
+    else
+    {
+        cout << "queue is not empty" << endl;
+    }
+
     // create a queue
+    /*
     queue<int> q;
 
     q.push(11);
@@ -33,4 +58,8 @@ int main()
         cout << "Queue is not empty "
              << "\n";
     }
+
+    */
+
+    return 0;
 }

c67fee42120313b0af0be7dc999ba40881b95a77
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-24 22:51:08 +0530

    bracketReversals question done

---
+++ Stacks/Questions/bracketReversal/main.cpp
@@ -0,0 +1,52 @@
+#include <stack>
+
+int findMinimumCost(string str)
+{
+    // odd condition
+    if (str.length() % 2 == 1)
+    {
+        return -1;
+    }
+
+    stack<char> s;
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+
+        if (ch == '{')
+        {
+            s.push(ch);
+        }
+        else
+        {
+            // ch is closed brace
+            if (!s.empty() && s.top() == '{')
+            {
+                s.pop();
+            }
+            else
+            {
+                s.push(ch);
+            }
+        }
+    }
+
+    // stack contains invalid expression
+    int a = 0, b = 0;
+
+    while (!s.empty())
+    {
+        if (s.top() == '{')
+        {
+            a++;
+        }
+        else
+        {
+            b++;
+        }
+        s.pop();
+    }
+
+    int result = ((a + 1) / 2) + ((b + 1) / 2);
+    return result;
+}

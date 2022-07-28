03c6ff69d58edfdcfcfd5f977b3c7f2f9323cf21
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-28 14:23:46 +0530

    pointer of array

--- Pointer/intro.cpp
+++ Pointer/intro.cpp
@@ -43,7 +43,6 @@ int main() {
 
     cout << p << endl;
     cout << *p << endl;
-    */
     
     int num = 5;
     int a = num;
@@ -73,6 +72,45 @@ int main() {
     cout << "before " << t << endl;
     t = t + 1;
     cout << "after " << t << endl;
+
+    int arr[10] = {23, 122, 41, 67};
+
+    cout << "address of first memory block is " << arr << endl;
+    cout << arr[0] << endl;
+    cout << "address of first memeory block is " << &arr[0] << endl;
+
+    cout << "4th " << *arr << endl;
+    cout << "5th " << *arr + 1 << endl;
+    cout << "6th " << *(arr + 1) << endl;
+    cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+    cout << "9th " << *(arr + 2) << endl;
+
+    int i = 3;
+    cout << i[arr] << endl;
+    
+    int temp[10] = {1, 2};
+    cout << sizeof(temp) << endl;
+    cout << "1st " << sizeof(*temp) << endl;
+    cout << "2nd " << sizeof(&temp) << endl;
+
+    int *ptr = &temp[0];
+    cout << sizeof(ptr) << endl;
+    cout << sizeof(*ptr) << endl;
+    cout << sizeof(&ptr) << endl;
+    
+    int a[20] = {1, 2, 3, 5};
+    cout << &a[0] << endl;
+    cout << &a << endl;
+    cout << a << endl;
+
+    int *p = &a[0];
+    cout << p << endl;
+    cout << *p << endl;
+    cout << &p << endl;
+    */
+    
+    
 
     return 0;
 }

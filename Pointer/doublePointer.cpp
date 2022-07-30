ffc9aec2c450997e83e9059ac847e7c1b8f03bdc
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-07-30 22:55:03 +0530

    complete mcq practice for pointers

--- Pointer/doublePointer.cpp
+++ Pointer/doublePointer.cpp
@@ -68,7 +68,6 @@ int main() {
     int *a = &first;
     cout << (*a)++ << endl;
     cout << first << endl;
-    */
 
     // 4.
     int *p = 0;
@@ -78,7 +77,115 @@ int main() {
     cout << *p << endl;
 
     // 5.
-    
+    int first = 8;
+    int second = 11;
+    int *third = &second;
+    first = *third;
+    *third = *third + 2;
+    cout << first << " " << second << endl;
 
+    // 6.
+    float f = 12.5;
+    float p = 21.5;
+    float* ptr = &f;
+    (*ptr)++;
+    *ptr = p;
+    cout << *ptr << " " << f << " " << p << endl;
+
+    // 7.
+    int arr[5];
+    int *ptr;
+    cout << sizeof(arr) << " " << sizeof(ptr) << endl;
+
+    // 8.
+    int arr[] = {11, 21, 13 , 14};
+    cout << *(arr) << " " << *(arr + 1) << endl;
+
+    // 9.
+    int arr[6] = {11, 12, 13};
+    cout << arr << " " << &arr << endl;
+    // arr, &arr[0] and &arr are the same -> address of arr[0]
+
+    // 10.
+    int arr[6] = {11, 21, 13};
+    cout << (arr + 1) << endl;
+
+    // 11.
+    int arr[6] = {11, 21, 31};
+    int *p = arr;
+    cout << p[2] << endl;
+
+    // 12.
+    int arr[] = {11, 12, 13, 14, 15};
+    cout << *(arr) << " " << *(arr + 3);
+
+    // 13.
+    int arr[] = {11, 21, 31, 41};
+    int *ptr = arr++; // reassignment during pointer declaration is not allowed as changes in the symbol table is not acceptable
+    cout << *ptr << endl;
+
+    // 14.
+    char ch = 'a';
+    char* ptr = &ch;
+    ch++;
+    cout << *ptr << endl;
+
+    // 15.
+    char arr[] = "abcde";
+    char *ptr = &arr[0];
+    cout << ptr << endl;
+
+    // 16.
+    char arr[] = "abcde";
+    char* ptr = &arr[0];
+    ptr++;
+    cout << ptr << endl;
+
+    // 17.
+    char str[] = "babbar";
+    char* ptr = str;
+    cout << str[0] << " " << ptr[0] << endl;
+    
+    // 18.
+    void update(int* ptr) {
+        *ptr = (*ptr) * 2;
+    }
+
+    int main() {
+        int i = 10;
+        update(&i);
+        cout << i << endl;
+        return 1;
+    }
+    
+    // 19.
+    int first = 110;
+    int *a = &first;
+    int **b = &a;
+    int second = (**b)++ + 9;
+    cout << first << " " << second << endl;
+    
+    // 20.
+    int first = 100;
+    int *a = &first;
+    int **b = &a;
+    int second = ++(**b);
+    int *ptr = *b;
+    ++(*ptr);
+    cout << first << " " << second << endl;
+
+    // 21.
+    void increment(int **ptr) {
+        ++(**ptr);
+    }
+
+    int main() {
+        int num = 110;
+        int *ptr = &num;
+        increment(&ptr);
+        cout << num << endl;
+    }
+    */
+    
     return 0;
 }

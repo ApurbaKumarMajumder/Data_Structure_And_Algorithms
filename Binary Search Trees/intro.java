d5f4ef86a5db034dd3f0aa443dd63bfc1d2d287d
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-08-08 03:36:49 +0530

    starting with BST

---
+++ Binary Search Trees/intro.java
@@ -0,0 +1,16 @@
+public class intro
+{
+	public static void main(String[] args) {
+		
+        // What is a BST
+            // * Binary Tree
+        
+            // a. Left Subtree Nodes < Root
+            // b. Right Subtree Nodes > Root
+            // c. Left & Right Subtrees are also BST with no duplicates.
+
+        // Special Property: Inorder Traversal of BST gives a sorted sequence.
+
+        
+	}
+}

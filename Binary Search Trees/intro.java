74c9688d3b08df5e1c4c918e0ca14fe61934a4d6
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-08-08 13:20:56 +0530

    delete and printInRange function done

--- Binary Search Trees/intro.java
+++ Binary Search Trees/intro.java
@@ -41,7 +41,7 @@ public class intro
         inorder(root.right);
     }
 
-    public static boolean search(Node root, int key) {
+    public static boolean search(Node root, int key) { // O(height)
 
         if (root == null) {
             return false;
@@ -57,6 +57,64 @@ public class intro
         } else {
             // right subtree
             return search(root.right, key);
+        }
+    }
+
+    public static Node delete(Node root, int val) {
+
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else if (root.data < val) {
+            root.right = delete(root.right, val);
+        }
+
+        else { // root.data == val
+            // case 1
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2
+            if (root.left == null) {
+                return root.right;
+            }
+            else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3
+            Node IS = inOrderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    public static Node inOrderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root;
+    }
+
+    public static void printInRange(Node root, int X, int Y) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (root.data >= X && root.data <= Y) {
+            printInRange(root.left, X, Y);
+            System.out.print(root.data + " ");
+            printInRange(root.right, X, Y);
+        }
+        else if (root.data >= Y) {
+            printInRange(root.left, X, Y);
+        }
+        else {
+            printInRange(root.right, X, Y);
         }
     }
 
@@ -87,5 +145,11 @@ public class intro
 
         boolean result = search(root, 6);
         System.out.println(result);
+
+        delete(root, 10);
+        inorder(root);
+
+        System.out.println();
+        printInRange(root, 6, 10);
 	}
 }

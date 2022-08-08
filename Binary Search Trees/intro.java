7efd0929ac0f414f5de93cb22ff855a89f023d5f
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-08-08 11:43:21 +0530

    inorder traversal in a BST

--- Binary Search Trees/intro.java
+++ Binary Search Trees/intro.java
@@ -30,6 +30,17 @@ public class intro
         return root;
     }
 
+    public static void inorder(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
 	public static void main(String[] args) {
 		
         // What is a BST
@@ -46,6 +57,13 @@ public class intro
         // Build a BST
 
         int values[] = {5, 1, 3, 4, 2, 7};
+        Node root = null;
 
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+        System.out.println();
 	}
 }

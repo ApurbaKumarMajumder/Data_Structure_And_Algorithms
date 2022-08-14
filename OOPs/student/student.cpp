3c6040335ddb01cdd2bf927e523aa2fe62742930
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-08-14 23:07:55 +0530

    getter and setter for a class

--- OOPs/student/student.cpp
+++ OOPs/student/student.cpp
@@ -1,8 +1,27 @@
+#include <iostream>
+using namespace std;
+
 class Student
 {
     public:
         int rollNumber;
+        
+    private:
         int age;
+
+    public:
+        void display() {
+            cout << age << " " << rollNumber << endl;
+        }
+
+        int getAge() {
+            return age;
+        }
+
+        int setAge(int a) {
+            age = a;
+            return age;
+        }
 };
 
 // int main() {
--- OOPs/student/studentUse.cpp
+++ OOPs/student/studentUse.cpp
@@ -14,17 +14,25 @@ int main()
     Student *s6 = new Student;
 
     // setting values of the properties of an object
-    s1.age = 24;
+    // s1.age = 24;
     s1.rollNumber = 100;
 
-    cout << s1.age << endl;
+    s1.display();
+    s2.display();
+
+    // cout << "S1 age : " << s1.getAge() << endl;
+    // cout << "S2 age : " << s1.getAge() << endl;
+
+    // cout << s1.age << endl;
     cout << s1.rollNumber << endl;
 
-    (*s6).age = 23;
+    // (*s6).age = 23;
     (*s6).rollNumber = 30;
+    (*s6).display();
 
-    s6 -> age = 23;
+    // s6 -> age = 23;
     s6 -> rollNumber = 30;
+    s6 -> display();
 
     return 0;
 }
